ec8df8273e6ce38ceff21733eab5d980190966e0
Tomoki108 <shinonome81@gmail.com>
2024-08-10 19:52:42 +0900

    create SheetResoisitory

--- ga_monitor/ga_monitor.ts
+++ ga_monitor/ga_monitor.ts
@@ -1,10 +1,16 @@
 // entry point
 async function gaMonitor() {
   const today = new Date();
-  const avgDuration = await getWorkflowRunAvgDuration("1", new Date());
-  const avgDurationLastTime = readLastAvgDurationLog().avgDuration;
+  const avgDuration = await getWorkflowRunAvgDuration("1", today);
 
-  writeAvgDurationLog({
+  const seetRepository = new SheetResoisitoryImpl(
+    SpreadsheetApp.getActiveSheet()
+  );
+
+  const avgDurationLastTime =
+    seetRepository.readLastAvgDurationLog().avgDuration;
+
+  seetRepository.writeAvgDurationLog({
     date: today,
     avgDuration: avgDuration,
     avgDurationDelta: avgDuration - avgDurationLastTime / avgDurationLastTime,
--- ga_monitor/spreadsheet.ts
+++ ga_monitor/spreadsheet.ts
@@ -2,33 +2,60 @@ const DATE_COLUMN = 1;
 const DURATION_COLUMN = 2;
 const DURATION_DELTA_COLUMN = 3;
 
-const sheet = SpreadsheetApp.getActiveSheet();
+// const sheet = SpreadsheetApp.getActiveSheet();
 
+// 平均実行時間ログ
 type AvgDurationLog = {
   date: Date;
   avgDuration: number; // 平均実行時間（秒）
   avgDurationDelta: number; // 前日比（%）
 };
 
-function readLastAvgDurationLog() {
-  const rangeToRead = sheet.getRange(sheet.getLastRow(), DATE_COLUMN, 1, 3);
-  const [date, avgDuration, avgDurationDelta] = rangeToRead.getValues()[0];
+/**
+ * スプレッドシートの操作を行うインターフェース
+ */
+interface SheetResoisitory {
+  /**
+   * 最新の平均実行時間ログを取得する
+   */
+  readLastAvgDurationLog(): AvgDurationLog;
 
-  return {
-    date: date,
-    avgDuration: avgDuration,
-    avgDurationDelta: avgDurationDelta,
-  } as AvgDurationLog;
+  /**
+   * 平均実行時間ログを書き込む
+   */
+  writeAvgDurationLog(log: AvgDurationLog): void;
 }
 
-function writeAvgDurationLog(log: AvgDurationLog) {
-  const rangeToWrite = sheet.getRange(
-    sheet.getLastRow() + 1,
-    DATE_COLUMN,
-    1,
-    3
-  );
-  rangeToWrite.setValue([log.date, log.avgDuration, log.avgDurationDelta]);
-}
+class SheetResoisitoryImpl implements SheetResoisitory {
+  private sheet: GoogleAppsScript.Spreadsheet.Sheet;
 
-function updateAvgDurationChart() {}
+  constructor(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    this.sheet = sheet;
+  }
+
+  readLastAvgDurationLog(): AvgDurationLog {
+    const rangeToRead = this.sheet.getRange(
+      this.sheet.getLastRow(),
+      DATE_COLUMN,
+      1,
+      3
+    );
+    const [date, avgDuration, avgDurationDelta] = rangeToRead.getValues()[0];
+
+    return {
+      date: date,
+      avgDuration: avgDuration,
+      avgDurationDelta: avgDurationDelta,
+    } as AvgDurationLog;
+  }
+
+  writeAvgDurationLog(log: AvgDurationLog): void {
+    const rangeToWrite = this.sheet.getRange(
+      this.sheet.getLastRow() + 1,
+      DATE_COLUMN,
+      1,
+      3
+    );
+    rangeToWrite.setValue([log.date, log.avgDuration, log.avgDurationDelta]);
+  }
+}
